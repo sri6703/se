c0ddba74ca1bd0e64a62e879efeb0bb3b29faea4
Priyanka1377 <priyakaruna2002@gmail.com>
2023-05-22 10:54:36 +0530

    neww

--- routes/login-page.js
+++ routes/login-page.js
@@ -15,7 +15,7 @@ router.use(express.urlencoded({ extended: false }));
 router.get('/',async (req,res) => {
     try {
         const logindet = await login.find()
-        res.json({logindet,mess:"Hello Friendsssss"})
+        res.json(logindet)
     } catch (err) {
         res.status(500).json({message: err.message})
     }
